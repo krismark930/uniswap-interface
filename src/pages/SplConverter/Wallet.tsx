8054f8d58a9046b55e1bd5e8d5df7edcf89223f8
happysmile911003 <happysmile911003@gmail.com>
2021-11-19 08:03:30 +0300

    added solana wallets

---
+++ src/pages/SplConverter/Wallet.tsx
@@ -0,0 +1,44 @@
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+    getPhantomWallet,
+    getSolletExtensionWallet,
+    getSolletWallet
+} from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+export const Wallet: FC = () => {
+    // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+    const network = WalletAdapterNetwork.Devnet;
+
+    // You can also provide a custom RPC endpoint
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
+    // Only the wallets you configure here will be compiled into your application
+    const wallets = useMemo(() => [
+        getPhantomWallet(),
+        getSolletWallet({ network }),
+        getSolletExtensionWallet({ network }),
+    ], [network]);
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <WalletMultiButton />
+                    <WalletDisconnectButton />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
---
+++ src/pages/SplConverter/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Wallet } from './Wallet'
+export default function SplConverter () {
+  return (
+    <div className='converter'>
+      <Wallet/>
+    </div>
+  )
+}
